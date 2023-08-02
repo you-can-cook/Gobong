a6f985fdc7f5ea89c9bcfd979b937cfd96ed08dd
uuranus <hyur0920@gmail.com>
2023-08-02 20:58:49 +0900

    [feat] 컬러 및 필요한 이미지와 기본 프래그먼트 구조 세팅

--- GoBong/app/src/main/java/com/youcancook/gobong/ui/bookmark/BookmarkFragment.kt
+++ GoBong/app/src/main/java/com/youcancook/gobong/ui/bookmark/BookmarkFragment.kt
@@ -1,4 +1,4 @@
-package com.youcancook.gobong.ui.notifications
+package com.youcancook.gobong.ui.bookmark
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.youcancook.gobong.databinding.FragmentNotificationsBinding
+import com.youcancook.gobong.databinding.FragmentBookmarkBinding
 
-class NotificationsFragment : Fragment() {
+class BookmarkFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentBookmarkBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -20,12 +20,12 @@ class NotificationsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View {
         val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+            ViewModelProvider(this).get(BookmarkViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentBookmarkBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textNotifications
--- GoBong/app/src/main/java/com/youcancook/gobong/ui/search/SearchViewModel.kt
+++ GoBong/app/src/main/java/com/youcancook/gobong/ui/search/SearchViewModel.kt
@@ -1,13 +1,13 @@
-package com.youcancook.gobong.ui.notifications
+package com.youcancook.gobong.ui.search
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class SearchViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is dashboard Fragment"
     }
     val text: LiveData<String> = _text
 }
