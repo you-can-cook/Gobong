1c0b37d97a12654ab62bfd2063c9ee1337d3f73d
donghoony <aru0504@naver.com>
2023-08-02 15:05:41 +0900

    feat: NotFound 예외 추가

--- backend/src/main/java/org/youcancook/gobong/global/error/ErrorCode.java
+++ backend/src/main/java/org/youcancook/gobong/global/error/ErrorCode.java
@@ -24,6 +24,18 @@ public enum ErrorCode {
     EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "A004", "만료된 토큰입니다."),
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "A005", "Unauthorized"),
 
+    // User
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "U004", "유저를 찾을 수 없습니다."),
+
+    // Recipe
+    RECIPE_NOT_FOUND(HttpStatus.NOT_FOUND, "R004", "레시피를 찾을 수 없습니다."),
+
+    // Recipe Details,
+    RECIPE_DETAIL_NOT_FOUND(HttpStatus.NOT_FOUND, "R104", "단계별 레시피를 찾을 수 없습니다."),
+
+    // Rating
+    RATING_NOT_FOUND(HttpStatus.NOT_FOUND, "R204", "평점을 찾을 수 없습니다."),
+
     ;
 
     private final HttpStatus status;
