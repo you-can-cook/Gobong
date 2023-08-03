cfbd1e316ec8caa0274da3a68fc5f56196ce9c11
donghoony <aru0504@naver.com>
2023-08-03 13:52:27 +0900

    feat: 평점 서비스 구현 및 테스트 작성 (#5)
    * feat: 북마크 엔티티 작성
    
    * feat: 평점 엔티티 구현
    
    * feat: 레시피 엔티티 구현
    
    * feat: 레시피 단계 엔티티 구현
    
    * fix: step 자료형 Integer로 변경, Nullable False 추가
    
    * fix: 자료형 통일
    
    * feat: BaseTime 구현
    
    * fix: 자료형 통일
    
    * fix: 자료형 통일 및 Rating 컬럼 제약사항 추가
    
    * feat: NotFound 예외 추가
    
    * feat: 평점 추가 / 업데이트 구현
    
    * delete: gitkeep 삭제
    
    * feat: 평점 범위 검증 로직 추가
    
    * test: 평점 검증 로직 유닛테스트 작성
    
    * merge: merge commit with backend branch
    
    * fix: 트랜잭션 어노테이션 추가
    
    * fix: Request DTO 상세 메시지 명시
    
    * test: 평점 서비스 테스트 작성
    
    * merge: Merge commit with backend branch
    
    * refactor: 불필요한 필드 제거
    
    * refactor: existsById로 수정

---
+++ backend/src/main/java/org/youcancook/gobong/domain/rating/service/RatingService.java
@@ -0,0 +1,47 @@
+package org.youcancook.gobong.domain.rating.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.youcancook.gobong.domain.rating.dto.response.RateRecipeResponse;
+import org.youcancook.gobong.domain.rating.entity.Rating;
+import org.youcancook.gobong.domain.rating.exception.RatingNotFoundException;
+import org.youcancook.gobong.domain.rating.repository.RatingRepository;
+import org.youcancook.gobong.domain.recipe.entity.Recipe;
+import org.youcancook.gobong.domain.recipe.exception.RecipeNotFoundException;
+import org.youcancook.gobong.domain.recipe.repository.RecipeRepository;
+import org.youcancook.gobong.domain.user.entity.User;
+import org.youcancook.gobong.domain.user.exception.UserNotFoundException;
+import org.youcancook.gobong.domain.user.repository.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class RatingService {
+
+    private final RatingRepository ratingRepository;
+    private final UserRepository userRepository;
+    private final RecipeRepository recipeRepository;
+
+    @Transactional
+    public RateRecipeResponse createRating(Long userId, Long recipeId, Integer score){
+        User user = userRepository.findById(userId).orElseThrow(UserNotFoundException::new);
+        Recipe recipe = recipeRepository.findById(recipeId).orElseThrow(RecipeNotFoundException::new);
+
+        Rating rating = new Rating(user, recipe, score);
+        Long ratingId = ratingRepository.save(rating).getId();
+        return new RateRecipeResponse(ratingId);
+    }
+
+    @Transactional
+    public void updateRating(Long userId, Long recipeId, Integer score){
+        if (!recipeRepository.existsById(recipeId)) {
+            throw new RecipeNotFoundException();
+        }
+
+        Rating rating = ratingRepository.findByUserIdAndRecipeId(userId, recipeId)
+                .orElseThrow(RatingNotFoundException::new);
+
+        rating.updateScore(score);
+    }
+
+}
--- backend/src/main/java/org/youcancook/gobong/global/error/ErrorCode.java
+++ backend/src/main/java/org/youcancook/gobong/global/error/ErrorCode.java
@@ -24,6 +24,19 @@ public enum ErrorCode {
     EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "A004", "만료된 토큰입니다."),
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "A005", "Unauthorized"),
 
+    // User
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "U004", "유저를 찾을 수 없습니다."),
+
+    // Recipe
+    RECIPE_NOT_FOUND(HttpStatus.NOT_FOUND, "R004", "레시피를 찾을 수 없습니다."),
+
+    // Recipe Details,
+    RECIPE_DETAIL_NOT_FOUND(HttpStatus.NOT_FOUND, "R104", "단계별 레시피를 찾을 수 없습니다."),
+
+    // Rating
+    RATING_RANGE_OUT_OF_BOUNDS(HttpStatus.BAD_REQUEST, "R201", "평점 범위는 1부터 5 사이입니다."),
+    RATING_NOT_FOUND(HttpStatus.NOT_FOUND, "R204", "평점을 찾을 수 없습니다.")
+
     ;
 
     private final HttpStatus status;
