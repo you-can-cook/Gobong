0afc5fd2ce7c08ed06620b3448ac9ffa981ed8da
uuranus <hyur0920@gmail.com>
2023-08-03 16:40:42 +0900

    [feat] 검색 화면 레이아웃

--- GoBong/app/src/main/java/com/youcancook/gobong/ui/search/SearchFragment.kt
+++ GoBong/app/src/main/java/com/youcancook/gobong/ui/search/SearchFragment.kt
@@ -4,35 +4,63 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import com.youcancook.gobong.R
+import com.youcancook.gobong.adapter.CardRecyclerViewListAdapter
+import com.youcancook.gobong.adapter.GridItemDecorator
+import com.youcancook.gobong.adapter.GridRecyclerViewListAdapter
 import com.youcancook.gobong.databinding.FragmentSearchBinding
+import com.youcancook.gobong.model.Recipe
 
 class SearchFragment : Fragment() {
 
     private var _binding: FragmentSearchBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    private val gridAdapter = GridRecyclerViewListAdapter()
+    private val gridItemDecorator =
+        GridItemDecorator()
+
+    private val linearAdapter = CardRecyclerViewListAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View {
         val dashboardViewModel =
             ViewModelProvider(this).get(SearchViewModel::class.java)
 
         _binding = FragmentSearchBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        return binding.root
+    }
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        binding.run {
+            recyclerView.layoutManager = GridLayoutManager(requireContext(), 3)
+            recyclerView.addItemDecoration(gridItemDecorator.also {
+                it.halfMargin =
+                    requireContext().resources.getDimension(R.dimen.grid_half_margin).toInt()
+            })
+            recyclerView.adapter = gridAdapter
         }
-        return root
+
+        gridAdapter.submitList(
+            listOf(
+                Recipe(),
+                Recipe(),
+                Recipe(),
+                Recipe(),
+                Recipe(),
+                Recipe(),
+                Recipe()
+            )
+        )
     }
 
     override fun onDestroyView() {
