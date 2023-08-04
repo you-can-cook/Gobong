069070eb871374e85d9bfe97f6fa096f9cd7d1ff
febbyviolen <febbyviolen@gmail.com>
2023-08-04 13:42:45 +0900

    프로젝트 생성
    [feat] 레이아웃

---
+++ Gobong/ViewController/BookmarkViewController.swift
@@ -0,0 +1,181 @@
+//
+//  SearchViewController.swift
+//  Gobong
+//
+//  Created by Ebbyy on 2023/08/02.
+//
+
+import UIKit
+import RxCocoa
+import RxSwift
+
+class BookmarkViewController: UIViewController {
+    
+    //property
+    private let searchBar = UISearchBar()
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var dummyData = [
+        dummyFeedData(username: "찝찝박사", following: true, thumbnailImg: "dummyImg", title: "맛있는 라면", bookmarkCount: 2, cookingTime: 3, tools: "냄비", level: "쉬워요", stars: 5),
+        dummyFeedData(username: "찝찝박사", following: true, thumbnailImg: "dummyImg", title: "맛있는 라면", bookmarkCount: 2, cookingTime: 3, tools: "냄비", level: "쉬워요", stars: 5),
+        dummyFeedData(username: "찝찝박사", following: true, thumbnailImg: "dummyImg", title: "맛있는 라면", bookmarkCount: 2, cookingTime: 3, tools: "냄비", level: "쉬워요", stars: 5),
+        dummyFeedData(username: "찝찝박사", following: true, thumbnailImg: "dummyImg", title: "맛있는 라면", bookmarkCount: 2, cookingTime: 3, tools: "냄비", level: "쉬워요", stars: 5)
+    ]
+    
+    private var ShowingBlockView = true
+    private var isShowingBlockView = PublishSubject<Bool>()
+    private var isShowingBlockViewObservable : Observable<Bool> {
+        return isShowingBlockView.asObservable()
+    }
+    
+    private let disposeBag = DisposeBag()
+    
+    override func viewWillAppear(_ animated: Bool) {
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        setObservable()
+        // Do any additional setup after loading the view.
+    }
+}
+
+//MARK: BUTTON FUNC
+extension BookmarkViewController {
+    @objc private func filterButtonTapped(){
+        
+    }
+    
+    @objc private func toogleButtonTapped(){
+        isShowingBlockView.onNext(!ShowingBlockView)
+    
+    }
+}
+
+//MARK: OBSERVABLE
+extension BookmarkViewController {
+    private func setObservable(){
+        isShowingBlockView.subscribe(onNext: { [weak self] bool in
+            guard let self = self else { return }
+            if bool {
+                ShowingBlockView = true
+                self.collectionView.isHidden = false
+                self.tableView.isHidden = true
+            } else {
+                ShowingBlockView = false
+                self.collectionView.isHidden = true
+                self.tableView.isHidden = false
+            }
+            
+        }).disposed(by: disposeBag)
+    }
+}
+
+//MARK: UI
+extension BookmarkViewController: UISearchBarDelegate {
+    private func setupUI(){
+        setupNavigationBar()
+        tableViewSetup()
+        collectionViewSetup()
+    }
+    
+    private func setupNavigationBar(){
+        navigationItem.titleView = searchBar
+        searchBar.backgroundColor = .white
+        searchBar.setImage(UIImage(), for: .search, state: .normal)
+        searchBar.searchBarStyle = .minimal
+        searchBar.text = "저장된 레시피"
+        if let textField = searchBar.value(forKey: "searchField") as? UITextField {
+//            textField.font = UIFont.boldSystemFont(ofSize: UIFont.systemFontSize)
+            textField.textAlignment = .center
+            textField.clearButtonMode = .never
+        }
+        searchBar.isUserInteractionEnabled = false
+        
+        
+        let tableViewToogleButton = UIBarButtonItem(image: UIImage(systemName: "square.stack.3d.up.fill"), style: .plain, target: self, action: nil)
+        tableViewToogleButton.tintColor = .white
+        let tableViewToogleButton2 = UIBarButtonItem(image: UIImage(systemName: "square.stack.3d.up.fill"), style: .plain, target: self, action: #selector(toogleButtonTapped))
+        tableViewToogleButton2.tintColor = .black
+        
+        navigationItem.leftBarButtonItem = tableViewToogleButton
+        navigationItem.rightBarButtonItem = tableViewToogleButton2
+    }
+}
+
+
+//MARK: COLLECTION VIEW
+extension BookmarkViewController : UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    private func collectionViewSetup(){
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(UINib(nibName: "FeedBoxCell", bundle: nil), forCellWithReuseIdentifier: "FeedBoxCell")
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return dummyData.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FeedBoxCell", for: indexPath) as! FeedBoxCell
+        
+        cell.img.image = UIImage(named: dummyData[indexPath.item].thumbnailImg)
+        
+        NSLayoutConstraint.activate([
+            cell.img.widthAnchor.constraint(equalToConstant: view.frame.width/3-2),
+            cell.img.heightAnchor.constraint(equalToConstant: view.frame.width/3-2)
+        ])
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        // Set the cell height to match the cellWidth so that the cell appears as a square
+        return CGSize(width: (view.frame.width/3)-2, height: view.frame.width/3-2)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 1)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+}
+
+//MARK: TABLEVIEW
+extension BookmarkViewController : UITableViewDelegate, UITableViewDataSource {
+    private func tableViewSetup(){
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.separatorStyle = .none
+        tableView.register(UINib(nibName: "FeedCell", bundle: nil), forCellReuseIdentifier: "FeedCell")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dummyData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FeedCell", for: indexPath) as! FeedCell
+        
+        let data = dummyData[indexPath.item]
+        cell.configuration(userImg: data.userImg, username: data.username, following: data.following, thumbnailImg: data.thumbnailImg, title: data.title, bookmarkCount: data.bookmarkCount, cookingTime: data.cookingTime, tools: data.tools, level: data.level, stars: data.stars)
+        cell.followingButton.titleLabel?.font = UIFont.systemFont(ofSize: 12) 
+        
+        cell.selectionStyle = .none
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 314.0
+    }
+    
+}
---
+++ Gobong/ViewController/SearchViewController.swift
@@ -0,0 +1,185 @@
+//
+//  SearchViewController.swift
+//  Gobong
+//
+//  Created by Ebbyy on 2023/08/02.
+//
+
+import UIKit
+import RxCocoa
+import RxSwift
+
+class SearchViewController: UIViewController {
+    
+    //property
+    private let searchBar = UISearchBar()
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var dummyData = [
+        dummyFeedData(username: "찝찝박사", following: true, thumbnailImg: "dummyImg", title: "라면", bookmarkCount: 2, cookingTime: 3, tools: "냄비", level: "쉬워요", stars: 5),
+        dummyFeedData(username: "찝찝박사", following: true, thumbnailImg: "dummyImg", title: "맛있는 라면", bookmarkCount: 2, cookingTime: 3, tools: "냄비", level: "쉬워요", stars: 5),
+        dummyFeedData(username: "찝찝박사", following: true, thumbnailImg: "dummyImg", title: "맛있는 라면", bookmarkCount: 2, cookingTime: 3, tools: "냄비", level: "쉬워요", stars: 5),
+        dummyFeedData(username: "찝찝박사", following: true, thumbnailImg: "dummyImg", title: "맛있는 라면", bookmarkCount: 2, cookingTime: 3, tools: "냄비", level: "쉬워요", stars: 5)
+    ]
+    
+    private var ShowingBlockView = true
+    private var isShowingBlockView = PublishSubject<Bool>()
+    private var isShowingBlockViewObservable : Observable<Bool> {
+        return isShowingBlockView.asObservable()
+    }
+    
+    private let disposeBag = DisposeBag()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        isShowingBlockView.onNext(true)
+    }
+
+    override func viewDidLoad() {
+        
+        // Do any additional setup after loading the view.
+        super.viewDidLoad()
+        setupUI()
+        setObservable()
+    }
+    
+}
+
+//MARK: BUTTON FUNC
+extension SearchViewController {
+    @objc private func filterButtonTapped(){
+        
+    }
+    
+    @objc private func toogleButtonTapped(){
+        isShowingBlockView.onNext(!ShowingBlockView)
+    
+    }
+}
+
+//MARK: OBSERVABLE
+extension SearchViewController {
+    private func setObservable(){
+        isShowingBlockView.subscribe(onNext: { [weak self] bool in
+            guard let self = self else { return }
+            if bool {
+                ShowingBlockView = true
+                
+                self.collectionView.isHidden = false
+                self.tableView.isHidden = true
+            } else {
+                ShowingBlockView = false
+                self.collectionView.isHidden = true
+                self.tableView.isHidden = false
+            }
+            
+        }).disposed(by: disposeBag)
+    }
+}
+
+//MARK: UI
+extension SearchViewController {
+    private func setupUI(){
+        searchBar.tintColor = .black
+        
+        setupNavigationBar()
+        tableViewSetup()
+        collectionViewSetup()
+        setupSearchBar()
+    }
+    
+    private func setupNavigationBar(){
+        navigationItem.titleView = searchBar
+        
+        let filterButton = UIBarButtonItem(image: UIImage(named: "Filter"), style: .plain, target: self, action: #selector(filterButtonTapped))
+        filterButton.tintColor = .black
+        
+        filterButton.imageInsets = UIEdgeInsets(top: 0.0, left: 50, bottom: 0, right: 30);
+
+        let tableViewToogleButton = UIBarButtonItem(image: UIImage(systemName: "square.stack.3d.up.fill"), style: .plain, target: self, action: #selector(toogleButtonTapped))
+        tableViewToogleButton.tintColor = .black
+        
+        navigationItem.rightBarButtonItems = [tableViewToogleButton, filterButton]
+    }
+}
+
+//MARK: SEARCH BAR
+extension SearchViewController {
+    private func setupSearchBar(){
+        searchBar.placeholder = "검색어를 입력하세요"
+    }
+}
+
+//MARK: COLLECTION VIEW
+extension SearchViewController : UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    private func collectionViewSetup(){
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(UINib(nibName: "FeedBoxCell", bundle: nil), forCellWithReuseIdentifier: "FeedBoxCell")
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return dummyData.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FeedBoxCell", for: indexPath) as! FeedBoxCell
+        
+        cell.img.image = UIImage(named: dummyData[indexPath.item].thumbnailImg)
+        
+        NSLayoutConstraint.activate([
+            cell.img.widthAnchor.constraint(equalToConstant: view.frame.width/3-2),
+            cell.img.heightAnchor.constraint(equalToConstant: view.frame.width/3-2)
+        ])
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        // Set the cell height to match the cellWidth so that the cell appears as a square
+        return CGSize(width: (view.frame.width/3)-2, height: view.frame.width/3-2)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 1)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+}
+
+//MARK: TABLEVIEW
+extension SearchViewController : UITableViewDelegate, UITableViewDataSource {
+    private func tableViewSetup(){
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.separatorStyle = .none
+        tableView.register(UINib(nibName: "FeedCell", bundle: nil), forCellReuseIdentifier: "FeedCell")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dummyData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FeedCell", for: indexPath) as! FeedCell
+        
+        let data = dummyData[indexPath.item]
+        cell.configuration(userImg: data.userImg, username: data.username, following: data.following, thumbnailImg: data.thumbnailImg, title: data.title, bookmarkCount: data.bookmarkCount, cookingTime: data.cookingTime, tools: data.tools, level: data.level, stars: data.stars)
+        cell.followingButton.titleLabel?.font = UIFont.systemFont(ofSize: 12) 
+        
+        cell.selectionStyle = .none
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 314.0
+    }
+    
+}
