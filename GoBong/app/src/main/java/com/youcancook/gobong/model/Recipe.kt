f1594e366e26bf972794d69f039a7a967d543e28
uuranus <hyur0920@gmail.com>
2023-08-05 17:51:17 +0900

    [feat] selector와 레시피 추가화면 MultiViewType으로

--- GoBong/app/src/main/java/com/youcancook/gobong/model/Recipe.kt
+++ GoBong/app/src/main/java/com/youcancook/gobong/model/Recipe.kt
@@ -1,15 +1,6 @@
 package com.youcancook.gobong.model
 
-data class Recipe(
-    val user: User = User(),
-    val thumbnailUrl: String = "",
-    val title: String = "내 레시피 대박임",
-    val bookmark: String = "0",
-    val time: String = "5분",
-    val tools: List<String> = listOf("전자레인지"),
-    val level: String = "쉬워요",
-    val star: String = "3.2공기",
-)
+sealed interface Recipe
 
 data class RecipeStep(
     val user: User = User(),
@@ -20,4 +11,15 @@ data class RecipeStep(
     val tools: List<String> = listOf("전자레인지"),
     val level: String = "쉬워요",
     val star: String = "3.2공기",
-)
+) : Recipe
+
+data class RecipeAdd(
+    val user: User = User(),
+    val thumbnailUrl: String = "",
+    val title: String = "내 레시피 대박임",
+    val bookmark: String = "0",
+    val time: String = "5분",
+    val tools: List<String> = listOf("전자레인지"),
+    val level: String = "쉬워요",
+    val star: String = "3.2공기",
+) : Recipe
