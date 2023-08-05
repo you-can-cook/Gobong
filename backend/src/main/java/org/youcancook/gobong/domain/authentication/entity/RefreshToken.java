a835fc74576edc607256bdf9e174b950bc77907f
seungtaekhong <hsk4991149@naver.com>
2023-08-05 22:34:21 +0900

    feat: RefreshToken에서 pk 생성 전략 삭제(직접 할당으로 변경)

--- backend/src/main/java/org/youcancook/gobong/domain/authentication/entity/RefreshToken.java
+++ backend/src/main/java/org/youcancook/gobong/domain/authentication/entity/RefreshToken.java
@@ -17,7 +17,6 @@ import java.util.Date;
 public class RefreshToken {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
 
     private String refreshToken;
