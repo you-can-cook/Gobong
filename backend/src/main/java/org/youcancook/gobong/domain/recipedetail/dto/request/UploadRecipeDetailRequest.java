169b5665469f910875cf93c9cd4e8c97337d596a
donghoony <aru0504@naver.com>
2023-08-05 20:04:59 +0900

    refactor: add missing request file

---
+++ backend/src/main/java/org/youcancook/gobong/domain/recipedetail/dto/request/UploadRecipeDetailRequest.java
@@ -0,0 +1,20 @@
+package org.youcancook.gobong.domain.recipedetail.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@ToString
+public class UploadRecipeDetailRequest {
+    private String content;
+    private String imageURL;
+
+    @NotNull(message = "조리시간은 필수 항목입니다.")
+    private Integer cookTimeInSeconds;
+
+    @NotNull(message = "조리도구는 필수 항목입니다.")
+    private Long cookware;
+}
