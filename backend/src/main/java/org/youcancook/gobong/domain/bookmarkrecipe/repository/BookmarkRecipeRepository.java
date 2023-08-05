bd4913c97da09a6fba868869f6ffe73d87985d5c
donghoony <aru0504@naver.com>
2023-08-05 15:48:44 +0900

    feat: 레시피 북마크 서비스 구현 (#12)
    * feat: 테이블 Column 제약사항 추가
    
    * feat: 레시피 북마크 추가/제거 기능 추가
    
    * test: 레시피 북마크 추가/제거 테스트 작성

--- backend/src/main/java/org/youcancook/gobong/domain/bookmarkrecipe/repository/BookmarkRecipeRepository.java
+++ backend/src/main/java/org/youcancook/gobong/domain/bookmarkrecipe/repository/BookmarkRecipeRepository.java
@@ -3,5 +3,11 @@ package org.youcancook.gobong.domain.bookmarkrecipe.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.youcancook.gobong.domain.bookmarkrecipe.entity.BookmarkRecipe;
 
+import java.util.Optional;
+
 public interface BookmarkRecipeRepository extends JpaRepository<BookmarkRecipe, Long> {
+    Optional<BookmarkRecipe> findByUserIdAndRecipeId(Long userId, Long recipeId);
+
+    Boolean existsByUserIdAndRecipeId(Long userId, Long recipeId);
+
 }
