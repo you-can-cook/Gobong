9f2c2c1cf281fb6d24b6d3d3ae053d3fe59153db
seungtaekhong <hsk4991149@naver.com>
2023-08-05 22:34:58 +0900

    refactor: 사용하지 않는 import문 삭제

--- backend/src/main/java/org/youcancook/gobong/domain/authentication/entity/RefreshToken.java
+++ backend/src/main/java/org/youcancook/gobong/domain/authentication/entity/RefreshToken.java
@@ -1,8 +1,6 @@
 package org.youcancook.gobong.domain.authentication.entity;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.Builder;
