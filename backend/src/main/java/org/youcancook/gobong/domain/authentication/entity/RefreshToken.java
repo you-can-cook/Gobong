9456c031191e6453c1e37605acd700e245675cc6
seungtaekhong <hsk4991149@naver.com>
2023-08-05 22:29:24 +0900

    feat: RefreshToken entity 및 repository 생성

---
+++ backend/src/main/java/org/youcancook/gobong/domain/authentication/entity/RefreshToken.java
@@ -0,0 +1,32 @@
+package org.youcancook.gobong.domain.authentication.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    private String refreshToken;
+    private Date expiredAt;
+
+    @Builder
+    public RefreshToken(Long userId, String refreshToken, Date expiredAt) {
+        this.userId = userId;
+        this.refreshToken = refreshToken;
+        this.expiredAt = expiredAt;
+    }
+}
