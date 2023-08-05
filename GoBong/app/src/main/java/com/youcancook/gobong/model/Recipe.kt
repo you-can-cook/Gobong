e3506bde504c4df2a56f38085bb70283d849de9b
uuranus <hyur0920@gmail.com>
2023-08-05 12:19:46 +0900

    [feat] 상세화면 레이아웃

---
+++ GoBong/app/src/main/java/com/youcancook/gobong/model/Recipe.kt
@@ -0,0 +1,23 @@
+package com.youcancook.gobong.model
+
+data class Recipe(
+    val user: User = User(),
+    val thumbnailUrl: String = "",
+    val title: String = "내 레시피 대박임",
+    val bookmark: String = "0",
+    val time: String = "5분",
+    val tools: List<String> = listOf("전자레인지"),
+    val level: String = "쉬워요",
+    val star: String = "3.2공기",
+)
+
+data class RecipeStep(
+    val user: User = User(),
+    val thumbnailUrl: String = "",
+    val title: String = "내 레시피 대박임",
+    val bookmark: String = "0",
+    val time: String = "5분",
+    val tools: List<String> = listOf("전자레인지"),
+    val level: String = "쉬워요",
+    val star: String = "3.2공기",
+)
--- GoBong/app/src/main/java/com/youcancook/gobong/ui/detail/DetailActivity.kt
+++ GoBong/app/src/main/java/com/youcancook/gobong/ui/detail/DetailActivity.kt
@@ -2,15 +2,27 @@ package com.youcancook.gobong.ui.detail
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.youcancook.gobong.adapter.RecipeListAdapter
 import com.youcancook.gobong.databinding.ActivityDetailBinding
+import com.youcancook.gobong.model.RecipeStep
 
 class DetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDetailBinding
-
+    private val recipeAdapter = RecipeListAdapter()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.run{
+            recyclerView.adapter = recipeAdapter
+        }
+
+        recipeAdapter.submitList(listOf(
+            RecipeStep(),
+            RecipeStep(),
+            RecipeStep()
+        ))
     }
 
 }
