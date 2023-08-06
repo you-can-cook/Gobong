aa36642a5f4cbd33b29491466f99374ea31c2593
febbyviolen <febbyviolen@gmail.com>
2023-08-06 15:56:21 +0900

    feat: detail view

---
+++ Gobong/ViewController/Cell/Hashtag/DetailTitleCell.swift
@@ -0,0 +1,23 @@
+//
+//  DetailTitleCell.swift
+//  Gobong
+//
+//  Created by Ebbyy on 2023/08/06.
+//
+
+import UIKit
+
+class DetailTitleCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
