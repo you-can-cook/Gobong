c9deb48deda786d532a0af35856276a6acf4269e
febbyviolen <febbyviolen@gmail.com>
2023-08-06 16:02:42 +0900

    feat: new feed + detail view

--- Gobong/ViewController/Cell/Hashtag/DetailTitleCell.swift
+++ Gobong/ViewController/Cell/Hashtag/DetailTitleCell.swift
@@ -9,15 +9,43 @@ import UIKit
 
 class DetailTitleCell: UITableViewCell {
 
+    @IBOutlet weak var background: UIView!
+    @IBOutlet weak var followingButton: UIButton!
+    @IBOutlet weak var starLabel: UILabel!
+    @IBOutlet weak var levelLabel: UILabel!
+    @IBOutlet weak var cookingToolsLabel: UILabel!
+    @IBOutlet weak var cookTimeLabel: UILabel!
+    @IBOutlet weak var thumbnailImage: UIImageView!
+    @IBOutlet weak var userProfile: UIImageView!
+    @IBOutlet weak var userName: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        followingButton.layer.cornerRadius = 2
+        followingButton.titleLabel?.adjustsFontSizeToFitWidth = true
+        followingButton.titleLabel?.font = UIFont.systemFont(ofSize: 12)
+        setBackgroundShadow()
     }
     
+    func setBackgroundShadow(){
+        background.layer.cornerRadius = 12
+        background.layer.shadowColor = UIColor(red: 0.721, green: 0.721, blue: 0.721, alpha: 0.25).cgColor
+        background.layer.shadowOpacity = 1
+        background.layer.shadowRadius = 10
+        background.layer.shadowOffset = CGSize(width: 0, height: 0)
+    }
+
+    func configuration(userImg: String? , username: String, following: Bool, thumbnailImg: String, title: String, bookmarkCount: Int, cookingTime: Int, tools: String, level: String, stars: Int) {
+        userProfile.image = UIImage(named: userImg ?? "프로필 이미지")
+        userName.text = username
+        followingButton.setTitle(following ? "팔로잉" : "팔로우", for: .normal)
+        thumbnailImage.image = UIImage(named: thumbnailImg)
+        cookTimeLabel.text = "\(cookingTime)분"
+        cookingToolsLabel.text = tools
+        levelLabel.text = level
+        starLabel.text = "\(stars)공기"
+        
+        
+    }
 }
