be60fad32f30b7df94a99daae196487b90ec86c0
uuranus <hyur0920@gmail.com>
2023-08-10 18:05:36 +0900

    [feat] 리뷰 작성 다이얼로그

--- GoBong/app/src/main/java/com/youcancook/gobong/ui/detail/DetailActivity.kt
+++ GoBong/app/src/main/java/com/youcancook/gobong/ui/detail/DetailActivity.kt
@@ -2,12 +2,15 @@ package com.youcancook.gobong.ui.detail
 
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.youcancook.gobong.adapter.RecipeListAdapter
 import com.youcancook.gobong.databinding.ActivityDetailBinding
+import com.youcancook.gobong.databinding.DialogReviewBinding
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
@@ -17,6 +20,9 @@ class DetailActivity : AppCompatActivity() {
     private val detailViewModel: DetailViewModel by viewModels()
     private val recipeAdapter = RecipeListAdapter(onItemClick = {
         detailViewModel.activeRecipeStep(it)
+    })
+    private val reviewDialog = ReviewDialogFragment(onDismissListener = {
+        detailViewModel.setStar(it)
     })
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,25 +41,9 @@ class DetailActivity : AppCompatActivity() {
                 finish()
             }
 
-            val stars = listOf(star1, star2, star3, star4, star5)
-
-            for ((index, star) in stars.withIndex()) {
-                star.setOnClickListener {
-                    detailViewModel.setStar(index + 1)
-                }
-            }
-
-            lifecycleScope.launch {
-                repeatOnLifecycle(Lifecycle.State.STARTED) {
-                    detailViewModel.starCount.collectLatest {
-                        stars.forEach { it2 -> it2.isSelected = false }
-
-                        for (i in 0 until it) {
-                            stars[i].isSelected = true
-                        }
-                    }
-                }
-
+            reviewButton.setOnClickListener {
+                reviewDialog.setOldStar(detailViewModel.getStar())
+                reviewDialog.show(supportFragmentManager, null)
             }
 
         }
@@ -61,13 +51,16 @@ class DetailActivity : AppCompatActivity() {
         binding.run {
             lifecycleScope.launch {
                 repeatOnLifecycle(Lifecycle.State.STARTED) {
-                    detailViewModel.activeRecipeStep.collectLatest {
-
+                    detailViewModel.starCount.collectLatest {
+                        if (it != 0) {
+                            reviewButton.text = "리뷰 수정하기"
+                        }
                     }
                 }
 
             }
         }
     }
+
 
 }
