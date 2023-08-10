4c0fa17e2e386a1cc777eb195b6a16acb1ddb5b9
SeungTaek Hong <51076814+redcarrot1@users.noreply.github.com>
2023-08-10 15:47:14 +0900

    feat: 회원가입 컨트롤러 생성 (#22)
    * feat: UserNickname validator 추가
    
    * feat: TemporaryTokenService 메서드 기능 변경 및 추가
    
    * test: temporaryTokenService 단위 테스트
    
    * refactor: 회원가입 서비스 클래스에서 닉네임 중복 검사 함수 분리
    
    * feat: signup 컨트롤러 생성
    
    * feat: User Entity의 nickname 컬럼 조건으로 unique 추가
    
    * test: 회원가입 인수 테스트

--- backend/src/main/java/org/youcancook/gobong/domain/user/dto/SignupDto.java
+++ backend/src/main/java/org/youcancook/gobong/domain/user/dto/SignupDto.java
@@ -3,6 +3,7 @@ package org.youcancook.gobong.domain.user.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import org.youcancook.gobong.domain.user.dto.request.SignupRequest;
 
 @Getter
 @Builder
@@ -13,4 +14,13 @@ public class SignupDto {
     private String oAuthProvider;
     private String oAuthId;
     private String profileImageURL;
+
+    public static SignupDto from(SignupRequest signupRequest) {
+        return SignupDto.builder()
+                .nickname(signupRequest.getNickname())
+                .oAuthProvider(signupRequest.getProvider())
+                .oAuthId(signupRequest.getOauthId())
+                .profileImageURL(signupRequest.getProfileImageURL())
+                .build();
+    }
 }
