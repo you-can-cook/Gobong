5712d10f54df3f008124b9dd226c37bdc671754c
donghoony <aru0504@naver.com>
2023-08-10 22:22:58 +0900

    feat: 레시피 삭제 기능 구현 (#24)
    * feat: 레시피 삭제 기능 구현
    
    * test: 레시피 삭제 테스트 작성
    
    * feat: 북마크도 함께 제거되도록 구현
    
    * test: Recipe 제거 시 북마크 제거 테스트 작성

--- backend/src/main/java/org/youcancook/gobong/domain/recipe/service/RecipeService.java
+++ backend/src/main/java/org/youcancook/gobong/domain/recipe/service/RecipeService.java
@@ -3,6 +3,8 @@ package org.youcancook.gobong.domain.recipe.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.youcancook.gobong.domain.bookmarkrecipe.repository.BookmarkRecipeRepository;
+import org.youcancook.gobong.domain.rating.repository.RatingRepository;
 import org.youcancook.gobong.domain.recipe.dto.request.CreateRecipeRequest;
 import org.youcancook.gobong.domain.recipe.dto.request.UpdateRecipeRequest;
 import org.youcancook.gobong.domain.recipe.dto.response.CreateRecipeResponse;
@@ -26,6 +28,8 @@ public class RecipeService {
     private final RecipeRepository recipeRepository;
     private final RecipeDetailRepository recipeDetailRepository;
     private final UserRepository userRepository;
+    private final RatingRepository ratingRepository;
+    private final BookmarkRecipeRepository bookmarkRecipeRepository;
 
     @Transactional
     public CreateRecipeResponse createRecipe(Long userId, CreateRecipeRequest request){
@@ -48,6 +52,18 @@ public class RecipeService {
         String parsedIngredients = String.join(",", request.getIngredients());
         recipe.updateProperties(request.getTitle(), request.getIntroduction(),
                 parsedIngredients, Difficulty.from(request.getDifficulty()), request.getThumbnailURL());
+    }
+
+    @Transactional
+    public void deleteRecipe(Long userId, Long recipeId){
+        User user = userRepository.findById(userId).orElseThrow(UserNotFoundException::new);
+        Recipe recipe = recipeRepository.findById(recipeId).orElseThrow(RecipeNotFoundException::new);
+        validateUserRecipe(user, recipe);
+
+        bookmarkRecipeRepository.deleteAllByRecipeId(recipeId);
+        recipeDetailRepository.deleteAllByRecipeId(recipeId);
+        ratingRepository.deleteAllByRecipeId(recipeId);
+        recipeRepository.delete(recipe);
     }
 
     public void validateUserRecipe(User user, Recipe recipe){
