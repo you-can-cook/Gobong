cc6ec1fe976dc2858c336008abdf396e6423957f
donghoony <aru0504@naver.com>
2023-08-11 03:06:32 +0900

    refactor: PK 복구 (refresh_token_id 제거)

--- backend/src/main/java/org/youcancook/gobong/domain/authentication/entity/RefreshToken.java
+++ backend/src/main/java/org/youcancook/gobong/domain/authentication/entity/RefreshToken.java
@@ -14,11 +14,7 @@ import java.util.Date;
 @Table(name = "refresh_token")
 public class RefreshToken {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "refresh_token_id")
-    private Long id;
-
-    @Column(unique = true)
+    @Id
     private Long userId;
 
     private String refreshToken;
