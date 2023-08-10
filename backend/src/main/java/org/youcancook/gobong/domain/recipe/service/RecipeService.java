1162597f01388aac5a094951d41316500f1ff1d1
donghoony <aru0504@naver.com>
2023-08-10 17:41:33 +0900

    feat: 레시피 삭제 기능 구현

--- backend/src/main/java/org/youcancook/gobong/domain/recipe/service/RecipeService.java
+++ backend/src/main/java/org/youcancook/gobong/domain/recipe/service/RecipeService.java
@@ -3,6 +3,7 @@ package org.youcancook.gobong.domain.recipe.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.youcancook.gobong.domain.rating.repository.RatingRepository;
 import org.youcancook.gobong.domain.recipe.dto.request.CreateRecipeRequest;
 import org.youcancook.gobong.domain.recipe.dto.request.UpdateRecipeRequest;
 import org.youcancook.gobong.domain.recipe.dto.response.CreateRecipeResponse;
@@ -26,6 +27,7 @@ public class RecipeService {
     private final RecipeRepository recipeRepository;
     private final RecipeDetailRepository recipeDetailRepository;
     private final UserRepository userRepository;
+    private final RatingRepository ratingRepository;
 
     @Transactional
     public CreateRecipeResponse createRecipe(Long userId, CreateRecipeRequest request){
@@ -48,6 +50,17 @@ public class RecipeService {
         String parsedIngredients = String.join(",", request.getIngredients());
         recipe.updateProperties(request.getTitle(), request.getIntroduction(),
                 parsedIngredients, Difficulty.from(request.getDifficulty()), request.getThumbnailURL());
+    }
+
+    @Transactional
+    public void deleteRecipe(Long userId, Long recipeId){
+        User user = userRepository.findById(userId).orElseThrow(UserNotFoundException::new);
+        Recipe recipe = recipeRepository.findById(recipeId).orElseThrow(RecipeNotFoundException::new);
+        validateUserRecipe(user, recipe);
+
+        recipeDetailRepository.deleteAllByRecipeId(recipeId);
+        ratingRepository.deleteAllByRecipeId(recipeId);
+        recipeRepository.delete(recipe);
     }
 
     public void validateUserRecipe(User user, Recipe recipe){
