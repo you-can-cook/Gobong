3a66888ae139e6767e38dbe596f8ecd05692bfda
donghoony <aru0504@naver.com>
2023-08-11 03:23:59 +0900

    test: 인수 테스트 Extend로 간소화

--- backend/src/test/java/org/youcancook/gobong/global/util/acceptance/AcceptanceTest.java
+++ backend/src/test/java/org/youcancook/gobong/global/util/acceptance/AcceptanceTest.java
@@ -2,27 +2,20 @@ package org.youcancook.gobong.global.util.acceptance;
 
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.youcancook.gobong.global.util.service.DatabaseCleaner;
-
-import static io.restassured.RestAssured.UNDEFINED_PORT;
+import org.youcancook.gobong.global.util.service.DatabaseCleanupExtension;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(DatabaseCleanupExtension.class)
 public class AcceptanceTest {
-
-    @Autowired
-    private DatabaseCleaner databaseCleaner;
 
     @LocalServerPort
     public int port;
+
     @BeforeEach
     void setUp(){
-        if (RestAssured.port == UNDEFINED_PORT){
-            RestAssured.port = port;
-            databaseCleaner.afterPropertiesSet();
-        }
-        databaseCleaner.execute();
+        RestAssured.port = port;
     }
 }
