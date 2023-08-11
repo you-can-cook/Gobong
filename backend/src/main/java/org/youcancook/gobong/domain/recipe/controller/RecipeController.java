aa77931b4116fedc531a552040351849be1caf53
donghoony <aru0504@naver.com>
2023-08-12 01:52:44 +0900

    feat: 레시피 컨트롤러 구현, 인수 테스트 작성 (#30)
    * feat: 레시피 컨트롤러 구현
    
    * test: 레시피 관련 인수 테스트 작성

---
+++ backend/src/main/java/org/youcancook/gobong/domain/recipe/controller/RecipeController.java
@@ -0,0 +1,40 @@
+package org.youcancook.gobong.domain.recipe.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.youcancook.gobong.domain.recipe.dto.request.CreateRecipeRequest;
+import org.youcancook.gobong.domain.recipe.dto.request.UpdateRecipeRequest;
+import org.youcancook.gobong.domain.recipe.dto.response.CreateRecipeResponse;
+import org.youcancook.gobong.domain.recipe.service.RecipeService;
+import org.youcancook.gobong.global.resolver.LoginUserId;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/recipes")
+public class RecipeController {
+
+    private final RecipeService recipeService;
+
+    @PostMapping
+    public ResponseEntity<CreateRecipeResponse> createRecipe(@LoginUserId Long userId,
+                                                             @RequestBody @Valid CreateRecipeRequest request){
+        CreateRecipeResponse response = recipeService.createRecipe(userId, request);
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("{recipeId}")
+    public ResponseEntity<Void> updateRecipe(@LoginUserId Long userId,
+                                             @RequestBody @Valid UpdateRecipeRequest request){
+        recipeService.updateRecipe(userId, request);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("{recipeId}")
+    public ResponseEntity<Void> deleteRecipe(@LoginUserId Long userId, @PathVariable Long recipeId){
+
+        recipeService.deleteRecipe(userId, recipeId);
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ backend/src/test/java/org/youcancook/gobong/domain/recipe/service/RecipeAcceptanceTest.java
@@ -0,0 +1,98 @@
+package org.youcancook.gobong.domain.recipe.service;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.youcancook.gobong.domain.recipe.dto.request.CreateRecipeRequest;
+import org.youcancook.gobong.domain.recipe.dto.request.UpdateRecipeRequest;
+import org.youcancook.gobong.domain.recipe.dto.response.CreateRecipeResponse;
+import org.youcancook.gobong.domain.recipedetail.dto.request.UploadRecipeDetailRequest;
+import org.youcancook.gobong.global.util.acceptance.AcceptanceTest;
+import org.youcancook.gobong.global.util.acceptance.AcceptanceUtils;
+
+import java.util.List;
+
+public class RecipeAcceptanceTest extends AcceptanceTest {
+
+    @Test
+    @DisplayName("레시피를 성공적으로 등록한다.")
+    public void createRecipe(){
+        String accessToken = AcceptanceUtils.signUpAndGetToken("쩝쩝박사", "KAKAO", "123");
+        CreateRecipeRequest request = new CreateRecipeRequest("주먹밥", "쉽게 만드는 주먹밥", List.of("밥", "김"), "쉬워요", null, List.of(
+                new UploadRecipeDetailRequest("소금간을 해 주세요", null, 30, 0L),
+                new UploadRecipeDetailRequest("주먹을 쥐어 밥을 뭉쳐주세요", null, 15, 0L)
+        ));
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .auth().oauth2(accessToken)
+                .body(request)
+                .when().post("/api/recipes")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .extract();
+    }
+
+    @Test
+    @DisplayName("레시피를 성공적으로 삭제한다.")
+    public void deleteRecipe(){
+        String accessToken = AcceptanceUtils.signUpAndGetToken("쩝쩝박사", "KAKAO", "123");
+        CreateRecipeRequest request = new CreateRecipeRequest("주먹밥", "쉽게 만드는 주먹밥", List.of("밥", "김"), "쉬워요", null, List.of(
+                new UploadRecipeDetailRequest("소금간을 해 주세요", null, 30, 0L),
+                new UploadRecipeDetailRequest("주먹을 쥐어 밥을 뭉쳐주세요", null, 15, 0L)
+        ));
+        Long recipeId = AcceptanceUtils.createDummyRecipe(accessToken, request).as(CreateRecipeResponse.class).getId();
+
+        RestAssured.given().log().all()
+                .auth().oauth2(accessToken)
+                .when().delete("/api/recipes/" + recipeId)
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .extract();
+    }
+
+    @Test
+    @DisplayName("권한없는 레시피 삭제 요청 시 올바른 에러코드를 반환한다.")
+    public void noAuth(){
+        String accessToken1 = AcceptanceUtils.signUpAndGetToken("쩝쩝박사", "KAKAO", "123");
+        String accessToken2 = AcceptanceUtils.signUpAndGetToken("쩝쩝학사", "GOOGLE", "123a");
+        CreateRecipeRequest request = new CreateRecipeRequest("주먹밥", "쉽게 만드는 주먹밥", List.of("밥", "김"), "쉬워요", null, List.of(
+                new UploadRecipeDetailRequest("소금간을 해 주세요", null, 30, 0L),
+                new UploadRecipeDetailRequest("주먹을 쥐어 밥을 뭉쳐주세요", null, 15, 0L)
+        ));
+        Long recipeId = AcceptanceUtils.createDummyRecipe(accessToken1, request).as(CreateRecipeResponse.class).getId();
+
+        RestAssured.given().log().all()
+                .auth().oauth2(accessToken2)
+                .when().delete("/api/recipes/" + recipeId)
+                .then().log().all()
+                .statusCode(HttpStatus.FORBIDDEN.value())
+                .extract();
+    }
+
+    @Test
+    @DisplayName("레시피를 성공적으로 수정한다.")
+    public void updateRecipe(){
+        String accessToken = AcceptanceUtils.signUpAndGetToken("쩝쩝박사", "KAKAO", "123");
+        CreateRecipeRequest request = new CreateRecipeRequest("주먹밥", "쉽게 만드는 주먹밥", List.of("밥", "김"), "쉬워요", null, List.of(
+                new UploadRecipeDetailRequest("소금간을 해 주세요", null, 30, 0L),
+                new UploadRecipeDetailRequest("주먹을 쥐어 밥을 뭉쳐주세요", null, 15, 0L)
+        ));
+        Long recipeId = AcceptanceUtils.createDummyRecipe(accessToken, request).as(CreateRecipeResponse.class).getId();
+        UpdateRecipeRequest updateRequest = new UpdateRecipeRequest(recipeId, "고소한 주먹밥", "쉽게 만드는 주먹밥",
+                List.of("밥", "김"), "쉬워요", null, List.of(
+                new UploadRecipeDetailRequest("소금간을 해 주세요", null, 30, 0L),
+                new UploadRecipeDetailRequest("주먹을 쥐어 밥을 뭉쳐주세요", null, 15, 0L)));
+
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(updateRequest)
+                .auth().oauth2(accessToken)
+                .when().put("/api/recipes/" + recipeId)
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .extract();
+    }
+}
