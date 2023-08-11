c4ea73dea2e7021ffd7499cb61a78e4bec977a51
uuranus <hyur0920@gmail.com>
2023-08-11 23:32:46 +0900

    [feat] token 저장 datasource -> sharedpreference로 변경

--- GoBong/app/src/main/java/com/youcancook/gobong/ui/RoutingActivity.kt
+++ GoBong/app/src/main/java/com/youcancook/gobong/ui/RoutingActivity.kt
@@ -8,35 +8,38 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
+import com.youcancook.gobong.MainActivity
 import com.youcancook.gobong.R
 import com.youcancook.gobong.ui.login.LoginActivity
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
 val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "token")
-val ACCESS_TOKEN = stringPreferencesKey("access_token")
+val ACCESS_TOKEN = "access_token"
 
 class RoutingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_routing)
-
-
     }
 
     override fun onStart() {
         super.onStart()
 
         //TODO 로그인 확인
-        val intent = Intent(this, LoginActivity::class.java)
-        startActivity(intent)
+        if (isTokenExpired()) {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        } else {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 
-    private fun isTokenExpired() {
-        val accessTokenFlow: Flow<String> = dataStore.data
-            .map { preferences ->
-                // No type safety.
-                preferences[ACCESS_TOKEN] ?: ""
-            }
+    private fun isTokenExpired(): Boolean {
+        getSharedPreferences(
+            ACCESS_TOKEN, Context.MODE_PRIVATE
+        ) ?: return false
+        return true
     }
 }
