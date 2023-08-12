6fc2a5f45e2afb54c45b2e2b1ef55fe81e5a7f6f
uuranus <hyur0920@gmail.com>
2023-08-12 11:02:08 +0900

    [feat] gitignore 다시 적용

--- GoBong/app/src/main/java/com/youcancook/gobong/GoBongApplication.kt
+++
@@ -1,6 +0,0 @@
-package com.youcancook.gobong
-
-import android.app.Application
-
-class GoBongApplication : Application() {
-}
--- GoBong/app/src/main/java/com/youcancook/gobong/model/Tool.kt
+++
@@ -1,7 +0,0 @@
-package com.youcancook.gobong.model
-
-data class Tool(
-    val toolName: String,
-    val isChecked: Boolean,
-    val isVisible: Boolean,
-)
