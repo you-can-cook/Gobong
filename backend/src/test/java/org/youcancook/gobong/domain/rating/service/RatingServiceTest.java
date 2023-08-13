30f2edac54a67ae55d76c612b00fee79e7ac09e9
donghoony <aru0504@naver.com>
2023-08-13 16:07:02 +0900

    test: 평점 테스트 작성

--- backend/src/test/java/org/youcancook/gobong/domain/rating/service/RatingServiceTest.java
+++ backend/src/test/java/org/youcancook/gobong/domain/rating/service/RatingServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.youcancook.gobong.domain.rating.entity.Rating;
+import org.youcancook.gobong.domain.rating.exception.RatingMyRecipeException;
 import org.youcancook.gobong.domain.rating.repository.RatingRepository;
 import org.youcancook.gobong.domain.recipe.entity.Difficulty;
 import org.youcancook.gobong.domain.recipe.entity.Recipe;
@@ -16,6 +17,8 @@ import org.youcancook.gobong.domain.user.repository.UserRepository;
 import org.youcancook.gobong.global.util.service.ServiceTest;
 
 import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 @ServiceTest
@@ -34,13 +37,17 @@ class RatingServiceTest {
     public void rateRecipeTest(){
         User user = User.builder().nickname("쩝쩝박사").oAuthId("123")
                 .oAuthProvider(OAuthProvider.GOOGLE).build();
-        Recipe recipe = Recipe.builder()
+        User user2 = User.builder().nickname("쩝쩝박사123").oAuthId("1234")
+                .oAuthProvider(OAuthProvider.GOOGLE).build();
+        Recipe recipe = Recipe.builder().user(user)
                 .title("주먹밥").difficulty(Difficulty.EASY).build();
 
         Long userId = userRepository.save(user).getId();
+        Long user2Id = userRepository.save(user2).getId();
+
         Long recipeId = recipeRepository.save(recipe).getId();
 
-        Long ratingId = ratingService.createRating(userId, recipeId, 3).getId();
+        Long ratingId = ratingService.createRating(user2Id, recipeId, 3).getId();
         Optional<Rating> actual = ratingRepository.findById(ratingId);
 
         Assertions.assertThat(actual).isNotEmpty();
@@ -52,20 +59,39 @@ class RatingServiceTest {
     public void updateRateTest(){
         User user = User.builder().nickname("쩝쩝박사").oAuthId("123")
                 .oAuthProvider(OAuthProvider.GOOGLE).build();
-        Recipe recipe = Recipe.builder()
+        User user2 = User.builder().nickname("쩝쩝박사123").oAuthId("1234")
+                .oAuthProvider(OAuthProvider.GOOGLE).build();
+        Recipe recipe = Recipe.builder().user(user)
+                .title("주먹밥").difficulty(Difficulty.EASY).build();
+
+        Long userId = userRepository.save(user).getId();
+        Long user2Id = userRepository.save(user2).getId();
+
+        Long recipeId = recipeRepository.save(recipe).getId();
+
+        Rating rating = new Rating(user2, recipe, 3);
+        Long ratingId = ratingRepository.save(rating).getId();
+
+        ratingService.updateRating(user2Id, recipeId, 5);
+        Optional<Rating> actual = ratingRepository.findById(ratingId);
+
+        Assertions.assertThat(actual).isNotEmpty();
+        Assertions.assertThat(actual.get().getScore()).isEqualTo(5);
+    }
+
+    @Test
+    @DisplayName("자신의 레시피에 평점을 남기려고 하면 예외를 발생한다.")
+    public void rateMyRecipe(){
+        User user = User.builder().nickname("쩝쩝박사").oAuthId("123")
+                .oAuthProvider(OAuthProvider.GOOGLE).build();
+        Recipe recipe = Recipe.builder().user(user)
                 .title("주먹밥").difficulty(Difficulty.EASY).build();
 
         Long userId = userRepository.save(user).getId();
         Long recipeId = recipeRepository.save(recipe).getId();
 
         Rating rating = new Rating(user, recipe, 3);
-        Long ratingId = ratingRepository.save(rating).getId();
-
-        ratingService.updateRating(userId, recipeId, 5);
-        Optional<Rating> actual = ratingRepository.findById(ratingId);
-
-        Assertions.assertThat(actual).isNotEmpty();
-        Assertions.assertThat(actual.get().getScore()).isEqualTo(5);
+        assertThrows(RatingMyRecipeException.class, () -> ratingService.createRating(userId, recipeId, 5));
     }
 
     @AfterEach
