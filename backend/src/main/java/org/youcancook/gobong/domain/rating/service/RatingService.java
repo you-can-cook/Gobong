f45b1ff1d19389bc3d280b8da4ca901549d5c253
donghoony <aru0504@naver.com>
2023-08-13 16:06:48 +0900

    feat: 자신의 레시피에 평점 남길 수 없도록 구현

--- backend/src/main/java/org/youcancook/gobong/domain/rating/service/RatingService.java
+++ backend/src/main/java/org/youcancook/gobong/domain/rating/service/RatingService.java
@@ -3,8 +3,9 @@ package org.youcancook.gobong.domain.rating.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.youcancook.gobong.domain.rating.dto.response.RateRecipeResponse;
+import org.youcancook.gobong.domain.rating.dto.response.AddRatingResponse;
 import org.youcancook.gobong.domain.rating.entity.Rating;
+import org.youcancook.gobong.domain.rating.exception.RatingMyRecipeException;
 import org.youcancook.gobong.domain.rating.exception.RatingNotFoundException;
 import org.youcancook.gobong.domain.rating.repository.RatingRepository;
 import org.youcancook.gobong.domain.recipe.entity.Recipe;
@@ -23,13 +24,14 @@ public class RatingService {
     private final RecipeRepository recipeRepository;
 
     @Transactional
-    public RateRecipeResponse createRating(Long userId, Long recipeId, Integer score){
+    public AddRatingResponse createRating(Long userId, Long recipeId, Integer score){
         User user = userRepository.findById(userId).orElseThrow(UserNotFoundException::new);
         Recipe recipe = recipeRepository.findById(recipeId).orElseThrow(RecipeNotFoundException::new);
+        validateSameUserRates(user, recipe);
 
         Rating rating = new Rating(user, recipe, score);
         Long ratingId = ratingRepository.save(rating).getId();
-        return new RateRecipeResponse(ratingId);
+        return new AddRatingResponse(ratingId);
     }
 
     @Transactional
@@ -44,4 +46,7 @@ public class RatingService {
         rating.updateScore(score);
     }
 
+    public void validateSameUserRates(User user, Recipe recipe){
+        if (user.getId().equals(recipe.getUser().getId())) throw new RatingMyRecipeException();
+    }
 }
