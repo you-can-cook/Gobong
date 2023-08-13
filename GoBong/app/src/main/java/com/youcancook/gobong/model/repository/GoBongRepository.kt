6c3f7b35896e8d6381cb5134039bfefea3ec937a
uuranus <hyur0920@gmail.com>
2023-08-13 15:11:58 +0900

    [feat] searchview style로 빼냄

--- GoBong/app/src/main/java/com/youcancook/gobong/model/repository/GoBongRepository.kt
+++ GoBong/app/src/main/java/com/youcancook/gobong/model/repository/GoBongRepository.kt
@@ -5,4 +5,12 @@ import com.youcancook.gobong.model.datasource.GoBongRemoteDataSource
 class GoBongRepository(
     private val goBongDataSource: GoBongRemoteDataSource,
 ) {
+
+    fun bookmarkRecipe(marked: Boolean) {
+        goBongDataSource.bookmarkRecipe(marked)
+    }
+
+    fun reviewRecipe(star: Int) {
+        goBongDataSource.reviewRecipe(star)
+    }
 }
