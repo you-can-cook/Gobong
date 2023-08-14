bc60f018196f74b6b5b5c59c8cddf5c9b506664f
donghoony <aru0504@naver.com>
2023-08-14 14:19:58 +0900

    refactor: 함수인자 제거

--- backend/src/main/java/org/youcancook/gobong/global/util/s3/AwsS3Controller.java
+++ backend/src/main/java/org/youcancook/gobong/global/util/s3/AwsS3Controller.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import org.youcancook.gobong.global.resolver.LoginUserId;
 
 import java.io.IOException;
 
@@ -19,8 +18,7 @@ public class AwsS3Controller {
     private final AwsS3ImageUploader awsS3ImageUploader;
 
     @PostMapping("upload")
-    public ResponseEntity<AwsS3ImageUploadResponse> upload(@LoginUserId Long userId,
-                                                           @RequestParam("image") MultipartFile file) throws IOException {
+    public ResponseEntity<AwsS3ImageUploadResponse> upload(@RequestParam("image") MultipartFile file) throws IOException {
         if (file.isEmpty()) throw new FileUploadException();
         String imageUrl = awsS3ImageUploader.uploadImage(file);
         AwsS3ImageUploadResponse response = new AwsS3ImageUploadResponse(imageUrl);
