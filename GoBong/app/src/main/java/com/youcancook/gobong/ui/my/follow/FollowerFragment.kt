fe0ec20a01f45cdc7fc2ad6f28c9f22df4829579
uuranus <hyur0920@gmail.com>
2023-08-14 23:25:20 +0900

    [feat] 팔로우,팔로잉 목록 비어있을 때 화면 추가

--- GoBong/app/src/main/java/com/youcancook/gobong/ui/my/follow/FollowerFragment.kt
+++ GoBong/app/src/main/java/com/youcancook/gobong/ui/my/follow/FollowerFragment.kt
@@ -5,48 +5,38 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.youcancook.gobong.R
 import com.youcancook.gobong.adapter.FollowListAdapter
 import com.youcancook.gobong.databinding.FragmentFollowerBinding
 import com.youcancook.gobong.model.User
+import com.youcancook.gobong.ui.base.NetworkFragment
+import com.youcancook.gobong.ui.base.NetworkStateListener
 
-class FollowerFragment : Fragment() {
-
-    private var _binding: FragmentFollowerBinding? = null
-    private val binding get() = _binding!!
+class FollowerFragment :
+    NetworkFragment<FragmentFollowerBinding, FollowViewModel>(R.layout.fragment_follower) {
     private val followAdapter = FollowListAdapter()
+    override val viewModel: FollowViewModel by lazy {
+        FollowViewModel(appContainer.userRepository)
+    }
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View {
-        _binding = FragmentFollowerBinding.inflate(inflater)
-        return binding.root
+    override val onNetworkStateChange: NetworkStateListener = object : NetworkStateListener {
+        override fun onSuccess() {
+
+        }
+
+        override fun onFail() {
+        }
+
+        override fun onDone() {
+        }
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.vm = viewModel
         super.onViewCreated(view, savedInstanceState)
         binding.run {
             recyclerView.adapter = followAdapter
         }
-
-        followAdapter.submitList(
-            listOf(
-                User(),
-                User(),
-                User(),
-                User(),
-                User(),
-                User(),
-                User(),
-                User(),
-
-                )
-        )
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 }
--- GoBong/app/src/main/java/com/youcancook/gobong/ui/my/follow/FollowingFragment.kt
+++ GoBong/app/src/main/java/com/youcancook/gobong/ui/my/follow/FollowingFragment.kt
@@ -5,48 +5,40 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.youcancook.gobong.R
 import com.youcancook.gobong.adapter.FollowListAdapter
 import com.youcancook.gobong.databinding.FragmentFollowingBinding
 import com.youcancook.gobong.model.User
+import com.youcancook.gobong.ui.base.NetworkFragment
+import com.youcancook.gobong.ui.base.NetworkStateListener
+import com.youcancook.gobong.util.NetworkState
 
-class FollowingFragment : Fragment() {
-
-    private var _binding: FragmentFollowingBinding? = null
-    private val binding get() = _binding!!
-    private val followAdapter = FollowListAdapter()
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View {
-        _binding = FragmentFollowingBinding.inflate(inflater)
-        return binding.root
+class FollowingFragment :
+    NetworkFragment<FragmentFollowingBinding, FollowViewModel>(R.layout.fragment_following) {
+    override val viewModel: FollowViewModel by lazy {
+        FollowViewModel(appContainer.userRepository)
     }
 
+    override val onNetworkStateChange: NetworkStateListener = object : NetworkStateListener {
+        override fun onSuccess() {
+        }
+
+        override fun onFail() {
+        }
+
+        override fun onDone() {
+        }
+
+    }
+
+    private val followAdapter = FollowListAdapter()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.vm = viewModel
         super.onViewCreated(view, savedInstanceState)
         binding.run {
             recyclerView.adapter = followAdapter
         }
-
-        followAdapter.submitList(
-            listOf(
-                User(),
-                User(),
-                User(),
-                User(),
-                User(),
-                User(),
-                User(),
-                User(),
-
-                )
-        )
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
 }
