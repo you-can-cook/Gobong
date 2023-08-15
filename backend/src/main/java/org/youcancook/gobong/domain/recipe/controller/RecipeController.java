17e7be8c1875a691dcde29dc96b2b095ddc1deee
donghoony <aru0504@naver.com>
2023-08-16 01:21:23 +0900

    feat: 레시피 단건조회 컨트롤러 구현

--- backend/src/main/java/org/youcancook/gobong/domain/recipe/controller/RecipeController.java
+++ backend/src/main/java/org/youcancook/gobong/domain/recipe/controller/RecipeController.java
@@ -7,7 +7,10 @@ import org.springframework.web.bind.annotation.*;
 import org.youcancook.gobong.domain.recipe.dto.request.CreateRecipeRequest;
 import org.youcancook.gobong.domain.recipe.dto.request.UpdateRecipeRequest;
 import org.youcancook.gobong.domain.recipe.dto.response.CreateRecipeResponse;
+import org.youcancook.gobong.domain.recipe.dto.response.GetRecipeResponse;
+import org.youcancook.gobong.domain.recipe.service.GetRecipeService;
 import org.youcancook.gobong.domain.recipe.service.RecipeService;
+import org.youcancook.gobong.domain.recipedetail.service.RecipeDetailService;
 import org.youcancook.gobong.global.resolver.LoginUserId;
 
 @RestController
@@ -16,11 +19,14 @@ import org.youcancook.gobong.global.resolver.LoginUserId;
 public class RecipeController {
 
     private final RecipeService recipeService;
+    private final RecipeDetailService recipeDetailService;
+    private final GetRecipeService getRecipeService;
 
     @PostMapping
     public ResponseEntity<CreateRecipeResponse> createRecipe(@LoginUserId Long userId,
                                                              @RequestBody @Valid CreateRecipeRequest request){
         CreateRecipeResponse response = recipeService.createRecipe(userId, request);
+        recipeDetailService.uploadRecipeDetails(response.getId(), request.getRecipeDetails());
         return ResponseEntity.ok(response);
     }
 
@@ -28,6 +34,7 @@ public class RecipeController {
     public ResponseEntity<Void> updateRecipe(@LoginUserId Long userId,
                                              @RequestBody @Valid UpdateRecipeRequest request){
         recipeService.updateRecipe(userId, request);
+        recipeDetailService.uploadRecipeDetails(request.getRecipeId(), request.getRecipeDetails());
         return ResponseEntity.ok().build();
     }
 
@@ -37,4 +44,11 @@ public class RecipeController {
         recipeService.deleteRecipe(userId, recipeId);
         return ResponseEntity.ok().build();
     }
+
+    @GetMapping("{recipeId}")
+    public ResponseEntity<GetRecipeResponse> getRecipe(@LoginUserId Long userId, @PathVariable Long recipeId){
+        GetRecipeResponse response = getRecipeService.getRecipe(userId, recipeId);
+        return ResponseEntity.ok(response);
+    }
+
 }
