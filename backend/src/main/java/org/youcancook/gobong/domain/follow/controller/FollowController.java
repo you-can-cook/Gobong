40c9f41ce93638107a9b129e0829bfdb2adf3350
SeungTaek Hong <51076814+redcarrot1@users.noreply.github.com>
2023-08-15 21:34:17 +0900

    feat: 팔로우, 팔로잉 API 생성 (#33)
    * feat: 팔로워, 팔로잉 서비스 생성
    
    * feat: 팔로워, 팔로잉 서비스 생성
    
    * feat: 팔로우, 팔로잉 컨트롤러 생성
    
    * test: 팔로우, 팔로잉 통합 테스트
    
    * refactor: 메소드 인자 순서 변경

--- backend/src/main/java/org/youcancook/gobong/domain/follow/controller/FollowController.java
+++ backend/src/main/java/org/youcancook/gobong/domain/follow/controller/FollowController.java
@@ -2,12 +2,13 @@ package org.youcancook.gobong.domain.follow.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.youcancook.gobong.domain.follow.dto.response.FindFolloweeResponse;
+import org.youcancook.gobong.domain.follow.dto.response.FindFollowerResponse;
 import org.youcancook.gobong.domain.follow.service.FollowService;
 import org.youcancook.gobong.global.resolver.LoginUserId;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -28,5 +29,17 @@ public class FollowController {
                                          @PathVariable Long folloeeId) {
         followService.unfollow(loginUserId, folloeeId);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("follower")
+    public ResponseEntity<List<FindFollowerResponse>> findFollowerList(@LoginUserId Long loginUserId) {
+        List<FindFollowerResponse> response = followService.findFollowerList(loginUserId);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("following")
+    public ResponseEntity<List<FindFolloweeResponse>> findFollowingList(@LoginUserId Long loginUserId) {
+        List<FindFolloweeResponse> response = followService.findFolloeeList(loginUserId);
+        return ResponseEntity.ok(response);
     }
 }
--- backend/src/main/java/org/youcancook/gobong/domain/follow/service/FollowService.java
+++ backend/src/main/java/org/youcancook/gobong/domain/follow/service/FollowService.java
@@ -3,6 +3,8 @@ package org.youcancook.gobong.domain.follow.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.youcancook.gobong.domain.follow.dto.response.FindFolloweeResponse;
+import org.youcancook.gobong.domain.follow.dto.response.FindFollowerResponse;
 import org.youcancook.gobong.domain.follow.entity.Follow;
 import org.youcancook.gobong.domain.follow.exception.AlreadyFollowingException;
 import org.youcancook.gobong.domain.follow.exception.NotFollowingException;
@@ -11,8 +13,11 @@ import org.youcancook.gobong.domain.user.entity.User;
 import org.youcancook.gobong.domain.user.exception.UserNotFoundException;
 import org.youcancook.gobong.domain.user.repository.UserRepository;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class FollowService {
 
     private final UserRepository userRepository;
@@ -50,5 +55,31 @@ public class FollowService {
         Follow follow = followRepository.findByFollowerAndFollowee(follower, followee)
                 .orElseThrow(NotFollowingException::new);
         followRepository.delete(follow);
+    }
+
+    public List<FindFollowerResponse> findFollowerList(Long loginUserId) {
+        User user = userRepository.findById(loginUserId)
+                .orElseThrow(UserNotFoundException::new);
+
+        return followRepository.findByFollowee(user).stream()
+                .map(follow -> FindFollowerResponse.of(
+                        follow.getFollower().getId(),
+                        follow.getFollower().getNickname(),
+                        follow.getFollower().getProfileImageURL()
+                ))
+                .toList();
+    }
+
+    public List<FindFolloweeResponse> findFolloeeList(Long loginUserId) {
+        User user = userRepository.findById(loginUserId)
+                .orElseThrow(UserNotFoundException::new);
+
+        return followRepository.findByFollower(user).stream()
+                .map(follow -> FindFolloweeResponse.of(
+                        follow.getFollowee().getId(),
+                        follow.getFollowee().getNickname(),
+                        follow.getFollowee().getProfileImageURL()
+                ))
+                .toList();
     }
 }
