ad7959a1067e4d5c4f9729fe4669facf27f4e5ae
seungtaekhong <hsk4991149@naver.com>
2023-08-15 19:08:27 +0900

    refactor: 사용하지 않는 import 문 삭제

--- backend/src/main/java/org/youcancook/gobong/domain/authentication/exception/InvalidRefreshTokenException.java
+++ backend/src/main/java/org/youcancook/gobong/domain/authentication/exception/InvalidRefreshTokenException.java
@@ -2,7 +2,6 @@ package org.youcancook.gobong.domain.authentication.exception;
 
 import org.youcancook.gobong.global.error.ErrorCode;
 import org.youcancook.gobong.global.error.exception.IllegalException;
-import org.youcancook.gobong.global.error.exception.NotFoundException;
 
 public class InvalidRefreshTokenException extends IllegalException {
     public InvalidRefreshTokenException() {
