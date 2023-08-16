34e887c760ddeada3d3d8322c359f72018577088
SeungTaek Hong <51076814+redcarrot1@users.noreply.github.com>
2023-08-16 14:11:40 +0900

    feat: 유저 정보 조회, 닉네임과 프로필 사진 변경 api (#43)
    * feat: 유저 조회, 정보 수정 api 생성
    
    * test: 유저 조회, 정보 수정 api 테스트
    
    * feat: 유저 조회, 정보 수정 api 생성
    
    * test: 유저 조회, 정보 수정 api 통합 테스트

---
+++ backend/src/test/java/org/youcancook/gobong/domain/user/service/UserInformationServiceTest.java
@@ -0,0 +1,93 @@
+package org.youcancook.gobong.domain.user.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.youcancook.gobong.domain.user.dto.response.UserInformationResponse;
+import org.youcancook.gobong.domain.user.entity.OAuthProvider;
+import org.youcancook.gobong.domain.user.entity.User;
+import org.youcancook.gobong.domain.user.exception.DuplicationNicknameException;
+import org.youcancook.gobong.domain.user.repository.UserRepository;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class UserInformationServiceTest {
+
+    @InjectMocks
+    private UserInformationService userInformationService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Test
+    @DisplayName("회원정보 조회")
+    void checkInformation() {
+        // given
+        User user = createTestUser();
+        ReflectionTestUtils.setField(user, "id", 1L);
+        when(userRepository.findById(user.getId()))
+                .thenReturn(Optional.of(user));
+
+        // when
+        UserInformationResponse result = userInformationService.getUserInformation(user.getId());
+
+        // then
+        assertThat(result.getId()).isEqualTo(user.getId());
+        assertThat(result.getNickname()).isEqualTo(user.getNickname());
+        assertThat(result.getOauthProvider()).isEqualTo(user.getOAuthProvider().name());
+        assertThat(result.getProfileImageURL()).isEqualTo(user.getProfileImageURL());
+    }
+
+    @Test
+    @DisplayName("회원정보 수정 성공")
+    void updateInformationSuccess() {
+        // given
+        String newNickname = "newNickname";
+        String newProfileImageURL = "newProfileImageURL";
+        User user = createTestUser();
+        Long userId = 1L;
+        when(userRepository.existsByNickname(newNickname))
+                .thenReturn(false);
+        when(userRepository.findById(userId))
+                .thenReturn(Optional.of(user));
+
+        // when
+        userInformationService.updateInformation(userId, newNickname, newProfileImageURL);
+
+        // then
+        assertThat(user.getNickname()).isEqualTo(newNickname);
+        assertThat(user.getProfileImageURL()).isEqualTo(newProfileImageURL);
+    }
+
+    @Test
+    @DisplayName("회원정보 수정 실패 - 중복된 닉네임")
+    void updateInformationFailByDuplicatedNickname() {
+        // given
+        String newNickname = "newNickname";
+        when(userRepository.existsByNickname(newNickname))
+                .thenReturn(true);
+
+        // expect
+        assertThrows(DuplicationNicknameException.class,
+                () -> userInformationService.updateInformation(1L, newNickname, "newProfileImageURL"));
+
+    }
+
+    private User createTestUser() {
+        return User.builder()
+                .nickname("nickname")
+                .oAuthProvider(OAuthProvider.KAKAO)
+                .oAuthId("oauthid")
+                .profileImageURL("profileImageURL")
+                .build();
+    }
+}
