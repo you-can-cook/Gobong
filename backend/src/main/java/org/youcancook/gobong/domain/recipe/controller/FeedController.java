e5156efd4623b493c84ce79b38025dd7e8ff5c91
donghoony <aru0504@naver.com>
2023-08-17 05:33:30 +0900

    fix: 피드 인자 순서 바뀐 것 수정

--- backend/src/main/java/org/youcancook/gobong/domain/recipe/controller/FeedController.java
+++ backend/src/main/java/org/youcancook/gobong/domain/recipe/controller/FeedController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.*;
 import org.youcancook.gobong.domain.recipe.dto.feedfilterdto.FeedFilterDto;
 import org.youcancook.gobong.domain.recipe.dto.feedfilterdto.ModifiedFeedFilterDto;
 import org.youcancook.gobong.domain.recipe.dto.feedfilterdto.QueryType;
-import org.youcancook.gobong.domain.recipe.dto.feedfilterdto.ModifiedFeedFilterDto;
-import org.youcancook.gobong.domain.recipe.dto.feedfilterdto.QueryType;
 import org.youcancook.gobong.domain.recipe.dto.response.GetFeedResponse;
 import org.youcancook.gobong.domain.recipe.service.GetRecipeService;
 import org.youcancook.gobong.global.resolver.LoginUserId;
@@ -23,7 +21,7 @@ public class FeedController {
     @GetMapping("all")
     public ResponseEntity<GetFeedResponse> getAllFeed(@LoginUserId Long userId,
                                                       @RequestParam(name = "last", required = false) Long lastRecipeId,
-                                                      @RequestParam(name = "count", required = true) int count){
+                                                      @RequestParam(name = "count") int count){
         lastRecipeId = lastRecipeId == null ? Long.MAX_VALUE : lastRecipeId;
         GetFeedResponse response = getRecipeService.getAllFeed(userId, lastRecipeId, count);
         return ResponseEntity.ok(response);
@@ -32,7 +30,7 @@ public class FeedController {
     @GetMapping("bookmarked")
     public ResponseEntity<GetFeedResponse> getBookmarkedFeed(@LoginUserId Long userId,
                                                    @RequestParam(name = "last", required = false) Long lastRecipeId,
-                                                   @RequestParam(name = "count", required = true) int count){
+                                                   @RequestParam(name = "count") int count){
         lastRecipeId = lastRecipeId == null ? Long.MAX_VALUE : lastRecipeId;
         GetFeedResponse response = getRecipeService.getBookmarkedFeed(userId, lastRecipeId, count);
         return ResponseEntity.ok(response);
@@ -41,7 +39,7 @@ public class FeedController {
     @GetMapping("following")
     public ResponseEntity<GetFeedResponse> getFollowingFeed(@LoginUserId Long userId,
                                                              @RequestParam(name = "last", required = false) Long lastRecipeId,
-                                                             @RequestParam(name = "count", required = true) int count){
+                                                             @RequestParam(name = "count") int count){
         lastRecipeId = lastRecipeId == null ? Long.MAX_VALUE : lastRecipeId;
         GetFeedResponse response = getRecipeService.getFollowingFeed(userId, lastRecipeId, count);
         return ResponseEntity.ok(response);
@@ -50,7 +48,7 @@ public class FeedController {
     @GetMapping("my")
     public ResponseEntity<GetFeedResponse> getMyFeed(@LoginUserId Long userId,
                                                      @RequestParam(name = "last", required = false) Long lastRecipeId,
-                                                     @RequestParam(name = "count", required = true) int count) {
+                                                     @RequestParam(name = "count") int count) {
         lastRecipeId = lastRecipeId == null ? Long.MAX_VALUE : lastRecipeId;
         GetFeedResponse response = getRecipeService.getMyFeed(userId, lastRecipeId, count);
         return ResponseEntity.ok(response);
@@ -60,7 +58,7 @@ public class FeedController {
     public ResponseEntity<GetFeedResponse> getUserFeed(@LoginUserId Long myId,
                                                        @PathVariable Long userId,
                                                        @RequestParam(name = "last", required = false) Long lastRecipeId,
-                                                       @RequestParam(name = "count", required = true) int count) {
+                                                       @RequestParam(name = "count") int count) {
         lastRecipeId = lastRecipeId == null ? Long.MAX_VALUE : lastRecipeId;
         GetFeedResponse response = getRecipeService.getUserFeed(userId, myId, lastRecipeId, count);
         return ResponseEntity.ok(response);
@@ -68,13 +66,13 @@ public class FeedController {
 
     @PostMapping("filter")
     public ResponseEntity<GetFeedResponse> getFilteredFeedByTitle(@LoginUserId Long userId,
-                                                           @RequestParam(name = "page", required = true) int page,
-                                                           @RequestParam(name = "count", required = true) int count,
+                                                           @RequestParam(name = "page") int page,
+                                                           @RequestParam(name = "count") int count,
                                                            @RequestBody @Valid FeedFilterDto filter){
         ModifiedFeedFilterDto modifiedFilter = ModifiedFeedFilterDto.from(filter);
         GetFeedResponse response = modifiedFilter.getQueryType() == QueryType.TITLE ?
-                getRecipeService.getFilteredFeedByName(userId, count, page, modifiedFilter) :
-                getRecipeService.getFilteredFeedByRecipeTitle(userId, count, page, modifiedFilter);
+                getRecipeService.getFilteredFeedByName(userId, page, count, modifiedFilter) :
+                getRecipeService.getFilteredFeedByRecipeTitle(userId, page, count, modifiedFilter);
         return ResponseEntity.ok(response);
     }
 
